39097a1a927994f1eeefd029e2090816967f62be
KeenGWatanabe <mckeenweb@gmail.com>
2025-08-13 17:27:26 +0800

    3:11:36

--- app/[...not-found]/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/[...not-found]/page.tsx
-export async function generateStaticParams() {
-  return [];
-}
-
-export default function NotFoundPage() {
-  return null; // Actual content handled by not-found.jsx
-}
