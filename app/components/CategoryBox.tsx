4291379ac9e14f9fd9af3479f5b5b2622c02cc11
KeenGWatanabe <mckeenweb@gmail.com>
2025-08-14 07:37:22 +0800

    disable useSearchParams

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -18,7 +18,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   selected
 }) => {
   const router = useRouter();
-  const params = useSearchParams();
+  // const params = useSearchParams();
 
   // const handleClick = useCallback(() => {
   //   let currentQuery = {};
