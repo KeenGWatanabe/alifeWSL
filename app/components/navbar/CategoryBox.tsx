61d76284a31da2565d1906fac97e6201dca098ca
KeenGWatanabe <mckeenweb@gmail.com>
2025-08-13 15:19:09 +0800

    3:08:03

--- app/components/navbar/CategoryBox.tsx
+++ app/components/navbar/CategoryBox.tsx
@@ -3,6 +3,7 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
   icon: IconType;
@@ -20,10 +21,32 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   const params = useSearchParams();
 
   const handleClick = useCallback(() => {
-    router.push(`/?category=${label}`);
-  }, [label, router]);
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      }
+
+      const updatedQuery: any = {
+        ...currentQuery,
+        category: label
+      };
+
+      if (params?.get('category') === label) {
+        delete updatedQuery.category;
+      }
+
+      const url = qs.stringifyUrl({
+        url: '/',
+        query: updatedQuery
+      }, { skipNull: true });
+
+      router.push(url);
+  }, [label, params, router]);
+
   return (
     <div 
+      onClick={handleClick}
       className={`
         flex 
         flex-col 
