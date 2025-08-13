396153d2b28179923cafc3576f6e3ce755b6c693
KeenGWatanabe <mckeenweb@gmail.com>
2025-08-13 16:48:21 +0800

    notfound.jsx

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+// app/not-found.jsx
+export default function NotFound() {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <h1 className="text-4xl font-bold text-rose-500">
+        404 - Page Not Found
+      </h1>
+    </div>
+  );
+}
