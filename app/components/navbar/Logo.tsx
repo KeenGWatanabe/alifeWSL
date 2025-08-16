73677e058e1c979366289dcda15c9814a4c1e297
KeenGWatanabe <mckeenweb@gmail.com>
2025-08-16 13:48:02 +0800

    3:15:47 rentModal.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -12,7 +12,8 @@ const Logo = () => {
         onClick={() => router.push('/')}
         alt="Logo"
         className="hidden md:block cursor-pointer"
-        sizes="(max-width: 768px) 100px, 100px" // Adjusted sizes
+        height={40}
+        width={40}
         src="/images/logo.png"
       /> 
     </div> 
